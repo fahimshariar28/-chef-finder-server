1794e547c409ea62c3264a5880723f60572962ad
Fahim Shariar <fahimshariar28@gmail.com>
2023-05-02 16:27:41 +0600

    json file added

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
+
+const chef = require("./data/chef.json");
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/chef", (req, res) => {
+  res.send(chef);
 });
 
 app.listen(port, () => {
